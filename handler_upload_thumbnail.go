ba2b9d27b1156ef6acd3e90b29fafb503680f361
Wolfy-22 <bubblarr@gmail.com>
2026-01-02 15:09:59 -0500

    Fixed Thumbnail Uploading

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +31,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error parsing thumbnail", err)
+		return
+	}
+
+	thumbnailData, thumbanilHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error forming thumbnail", err)
+		return
+	}
+
+	contentType := thumbanilHeader.Header.Get("Content-Type")
+
+	tBytes, err := io.ReadAll(thumbnailData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error reading bytes", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video Does not Exist", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not owner of video", err)
+		return
+	}
+
+	videoThumbnails[video.ID] = thumbnail{data: tBytes, mediaType: contentType}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:<8091>/api/thumbnails/{%v}", video.ID)
+
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
+		return
+	}
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
-
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	respondWithJSON(w, http.StatusOK, video)
 }
